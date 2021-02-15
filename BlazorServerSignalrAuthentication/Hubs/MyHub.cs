c79654df52dd51028a121969e720745011d7e045
aadurham <atila.abdulkadiroglu@gmail.com>
2021-02-15 15:45:32 -0500

    Initial Commit
    It contains a Signalr hub and a simple method that is invoked by the user. The [AUthorize] attribute on the hub crashes the code so it is commented out. The goal is to get User.Identity.Name in the hub. It is not getting it at the moment.

---
+++ BlazorServerSignalrAuthentication/Hubs/MyHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerSignalrAuthentication.Hubs
+{
+    //[Authorize]
+    public class MyHub : Hub
+    {
+        public async Task InvokeSomething()
+        {
+            var name = Context.GetHttpContext().User.Identity.Name?? "NameCouldNotBeLoaded";
+            await Clients.All.SendAsync("ReceiveMessage", name, " invoked this");
+        }
+
+    }
+}
